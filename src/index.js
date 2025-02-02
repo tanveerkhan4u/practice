cda2d2a5ab1a9b8930de143b7e0c2f47aef4cff4
tanveerkhan4u <khantanveer6077@gmail.com>
2025-02-02 20:36:47 +0530

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,41 @@
 // console.log(arr);
 
 // hoisting
+// hositing concept work with var not with let and const
+
+// function hoisting
+
+// add();
+
+// function add(){
+//     console.log("Hello World");
+// }
+
+// const add = function(){
+//     console.log("Hello World");
+// }
+
+// arrow function hoisting
+
+// add();
+
+// var add = () => {
+//     console.log("Hello World");
+
+// }
+// add();
+ 
+// var add =  function(){
+//     console.log("Hello World");
+// }
+
+
+
+// yani ki maine function ko as a proper function likja hai tbbhi hositing work krega vrna function ko expression function likha hai to hositing work nhi krega ya arrow function likha hai to hositing work nhi krega
+
+
+
+
 
 
 
