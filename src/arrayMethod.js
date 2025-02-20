33b3d75488b2219e2a6b9c88e71e62ae4819734b
tanveerkhan4u <khantanveer6077@gmail.com>
2025-02-20 20:46:53 +0530

    update

---
+++ src/arrayMethod.js
@@ -0,0 +1,40 @@
+// concat method mean concatination
+// ye use hota hai jab dono array ko combine krke new array bnana ho aur array me value add krni ho vo bhi ho sakta hai
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = ["a", "b", "c", "d", "e"];
+
+// const newArr = arr1.concat(arr2);
+// console.log(newArr);
+
+// const arr1 = [1, 2, 3, 4, 5];
+// const arr2 = ["tanveer", "ahmed", "khan"];
+
+
+// const newArr = arr2.concat(arr1);
+// console.log(newArr);
+
+// const arr1 = [1, 2, 3, 4, 5];
+
+// const NewArr = arr1.concat(100);
+// console.log(NewArr);
+
+
+// join method
+// array ke element ko jodh ke string banta hai mtlb uske beech me seperator mtlb question mark ya space kuch bhi daal ke join krte hai usko string bana sakte hai simple elementr ko milake string bana sakte hai ye string return karta hai as a seperator  use krta hai jo string me add krna ho vo add krta hai like comma space question mark etc
+
+// const arr2 = ["tanveer", "ahmed", "khan"];
+// const arr1 = [1, 2, 3, 4, 5];
+
+// const newArr = arr2.join("  ");
+// console.log(newArr);
+
+// const arr1 = ["how", "are", "you", "baby"];
+
+// const joinMethod = arr1.join(" ");
+//     console.log(joinMethod);
+
+// Slice Method
+// slice mtlb tukda krna part krna means it is used to extract a part of array and return a new array without modifying the original array 
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
---
+++ src/closure.js
@@ -0,0 +1,10 @@
+function main(){
+    const name = 'tanveer';
+
+    function sayMyName(){
+        console.log(name);
+        
+    }
+    sayMyName();
+}
+main();
