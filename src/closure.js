06da27f094ee5825bf5f6176eef6a2c414095c5e
tanveerkhan4u <khantanveer6077@gmail.com>
2025-02-20 23:44:14 +0530

    update

--- src/closure.js
+++ src/closure.js
@@ -9,3 +9,16 @@
 // }
 // main();
 
+// closure means that inner function has always access to the variables and parameters of the outer function, even after the outer function.
+
+function a(){
+    let name = 'tanveer'
+    
+
+    function b(){
+        console.log(name)
+    }
+    b()
+}
+
+a()
